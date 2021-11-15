3e84907609d23f5152ee27182582f89406e9fd3e
Maxime LABELLE <maxime.labelle@chanel.com>
2021-11-15 14:44:21 +0100

    Support for plugins.

---
+++ Plugin/HelloPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Credentials.Interop;
+
+namespace HelloPlugin
+{
+    public class HelloCommand : ICredentialStore
+    {
+        public bool RemoveCredentials(string target)
+        {
+            Console.WriteLine("Hello !!!");
+            return true;
+        }
+
+        public bool SetCredentials(string target, string username, string password)
+        {
+            Console.WriteLine("Hello !!!");
+            return true;
+        }
+    }
+}
---
+++ PluginBase/ICredentialStore.cs
@@ -0,0 +1,8 @@
+﻿namespace Credentials.Interop
+{
+	public interface ICredentialStore
+	{
+		bool SetCredentials(string target, string username, string password);
+		bool RemoveCredentials(string target);
+	}
+}
